c21e9183aaf3047c0eacff9afc3878beb353edca
Hayk Davoyan <hayk.davoyan.2004@gmail.com>
2024-02-22 21:42:15 +0400

    Add project files.

---
+++ ChessBoard/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ChessBoard
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point position = e.GetPosition((IInputElement)sender);
+
+            X.Content = $"X: {position.X.ToString("0")}";
+            Y.Content = $"Y: {position.Y.ToString("0")}";
+        }
+    }
+}
